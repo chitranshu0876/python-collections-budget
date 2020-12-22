52c00c060ebcbae351da0ad4cce35bd7b51b254d
Chitranshu Srivastava <chitranshu@gmail.com>
2020-12-22 18:26:26 +0800

    reading expenses and graphing them and now making it similar

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -7,13 +7,15 @@ expenses.read_expenses("data/spending_data.csv")
 spending_categories=[]
 for expense in expenses.list:
 	spending_categories.append(expense.category)
+
 spending_counter=collections.Counter(spending_categories)
 ##print(spending_counter)
 top5=spending_counter.most_common(5)
 categories, count=zip(*top5)
-fig, ax=plt.subplots()
+
+fix, ax=plt.subplots()
 ax.bar(categories,count)
-ax.set_title("# of purchases by category")
+ax.set_title('# of purchases by category')
 plt.show()
 
 
